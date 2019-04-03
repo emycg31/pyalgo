d73b86344b38a263ba4db0d14a2ffa0b0c18f189
EmilieR <emilie.univtlse3@gmail.com>
2019-04-03 11:51:27 +0200

    Mise à jour des commentaires

---
+++ hello.py
@@ -0,0 +1,14 @@
+"""
+    Algorithm #0.01
+    Declare var
+    Store some arithmetics into the var
+    Display result
+    Operands and Operators, functions
+"""
+resultat = 0 # Définition de la variable
+resultat = 2 + 2
+print(resultat)
+
+"""
+Fin de l'algorithme
+"""
