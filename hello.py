38f6edeac14e377382de9f5363f197acfa649e40
EmilieR <emilie.univtlse3@gmail.com>
2019-04-03 13:50:25 +0200

    Signe du résultat

--- hello.py
+++ hello.py
@@ -10,9 +10,16 @@
 """
 resultat = 0 # Définition de la variable
 operande1 = -3
-operande2 = 2
+operande2 = 3
 resultat = operande1 + operande2
-print(resultat)
+
+if resultat > 0: 
+    print("Le résultat est positif")
+elif resultat < 0: 
+    print("Le résultat est négatif")
+else:
+    print("Le résultat est nul")
+#print(resultat)
 
 """
 Fin de l'algorithme
