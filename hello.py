b033ce67329fb0fbcda44015e038cb422bde32f9
EmilieR <emilie.univtlse3@gmail.com>
2019-04-04 17:06:24 +0200

    Manip tableaux, fonctions

--- hello.py
+++ hello.py
@@ -12,6 +12,7 @@ resultat = 0 # Définition de la variable
 operande1 = -3
 operande2 = 3
 resultat = operande1 + operande2
+print(resultat)
 
 if resultat > 0: 
     print("Le résultat est positif")
@@ -24,3 +25,35 @@ else:
 """
 Fin de l'algorithme
 """
+
+"""
+FUNCTION SETTING
+"""
+def addition(operande1, operande2):
+    return operande1 + operande2
+
+resultat = 0 # Définition de la variable
+operande1 = -3
+operande2 = 2
+resultat = addition(5,3) #call function with two parameters
+print(resultat)
+
+"""
+getLowerOf
+return the lowest value of two params
+"""
+def getLowerOf(firstVal, secondVal):
+    if firstVal < secondVal:
+        return firstVal
+    else:
+        return secondVal
+
+"""
+getGreaterOf
+return the geatest value of two params
+"""
+def getGreaterOf(firstVal, secondVal):
+    if firstVal > secondVal:
+        return firstVal
+    else:
+        return secondVal
