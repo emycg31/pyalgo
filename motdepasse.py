a71b87a042309da834e5bd990b0201a77e599e9a
EmilieR <emilie.univtlse3@gmail.com>
2019-04-05 13:51:55 +0200

    Générateur de mot de passe aléatoire

--- motdepasse.py
+++ motdepasse.py
@@ -3,6 +3,11 @@
 @author Aélion
 @version 1.0.0
     Algorithmique de génération aléatoire de mot de passe
+@contrainte1 : min 8 caractères, max 12
+@contrainte2 : au moins 1 lettre majuscule
+@contrainte3 : au moins 1 caractère de ponctuation
+@contrainte4 : 1 chiffre
+
 """
 
 # Tableaux
@@ -12,9 +17,10 @@ tabNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 tabMinletters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 tableauPrinc = [tabLetters, tabPonct, tabNumbers, tabMinletters]
 
+import random
 
-from random import *
-nbCaract = int(uniform(8,13))
+# Choix de la longueur du mot de passe
+nbCaract = random.randint(8,12)
 print("Nombre de caractère :", nbCaract)
 
 letter = str()
@@ -25,9 +31,9 @@ passwordint = str()
 passwordf = str()
 
 # Déterminer les caractères obligatoires
-indiceL = int(uniform(0,len(tableauPrinc[0])))
-indiceP = int(uniform(0,len(tableauPrinc[1])))
-indiceN = int(uniform(0,len(tableauPrinc[2])))
+indiceL = random.randint(0,len(tableauPrinc[0])-1)
+indiceP = random.randint(0,len(tableauPrinc[1]))-1
+indiceN = random.randint(0,len(tableauPrinc[2])-1)
 
 letter = tableauPrinc[0][indiceL]
 ponct = tableauPrinc[1][indiceP]
@@ -38,10 +44,10 @@ print("Letter :", letter, "Ponct :", ponct, "Number :", number)
 # Déterminer les caractères restants
 if len(passwordtr) < nbCaract-3:
     for i in range(nbCaract-3):
-        whichType = int(uniform(0,4))
+        whichType = random.randint(0,3)
         print("Choix du tableau :", whichType)
         #print("Tableau choisi :", tableauPrinc[whichType])
-        indice = int(uniform(0,len(tableauPrinc[whichType])))
+        indice = random.randint(0,len(tableauPrinc[whichType])-1)
         #print("indice :", indice)
         #print(type(i))
         #print("Valeur :", val)
@@ -50,9 +56,10 @@ if len(passwordtr) < nbCaract-3:
         passwordtr = passwordtr + val
 print("Mot de passe tronqué :", passwordtr)
 
+# Concaténation des caractères obligatoires et non obligatoires
 passwordint = passwordtr + letter + ponct + number
 print("Mot de passe concaténé:", passwordint)
 
-import random
+# Mélange de tous les caractères respectant les contraintes
 passwordf = "".join(random.sample(passwordint,len(passwordint)))
 print("Mot de passe final:", passwordf)
